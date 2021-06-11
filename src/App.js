fe5f76db89285d82a8abb80159601b60d1263407
hwlv <3025947168@qq.com>
2021-06-11 14:11:38 +0800

    1 file modified

--- src/App.js
+++ src/App.js
@@ -19,24 +19,28 @@ const Log = props => {
 };
 
 const Condition = props => {
-  const { count } = props;
+  const { count, condition } = props;
 
+  // useEffect(() => {
+  //   console.log('Condition count');
+  //   console.log(count);
+  //   if (count === 1) return;
+  // }, [count]);
   useEffect(() => {
-    console.log('Condition count');
-    console.log(count);
-    if (count === 1) return;
-  }, [count]);
-
+    console.log(condition);
+  }, [condition]);
   return (
     <div>
-      <h1>condition count: {count}</h1>
-      <Log count={count} />
+      {/* <h1>condition count: {count}</h1>*/}
+      <h1>condition {condition.name}</h1>
+      {/* <Log count={count} />  */}
     </div>
   );
 };
 
 export default function App() {
   const [count, setCount] = useState(0);
+  const [condition, setCondition] = useState({ name: 'jack' });
 
   useEffect(() => {
     console.log(count);
@@ -48,8 +52,10 @@ export default function App() {
       <Button onClick={() => setCount(count + 1)} type="primary">
         Click me
       </Button>
-
-      <Condition count={count} />
+      <Button onClick={() => setCondition({ name: 'tom' })} type="primary">
+        setCondition
+      </Button>
+      <Condition count={count} condition={condition} />
     </div>
   );
 }
